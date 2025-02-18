47299b48f2a90415b4f259fb7da054d64fd78daf
nepodude <nepo.jean73@gmail.com>
2025-02-18 17:37:43 +0200

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/pup
 # This Puppet script installs Flask version 2.1.0 using pip3
 
-package { 'Flask':
+package {'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
